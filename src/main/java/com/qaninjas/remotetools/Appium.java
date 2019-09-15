72378bedb7f7b8b784c82dab2362eb05b6476233
manishleoy2k3 <manishleoy2k3@gmail.com>
2019-09-15 08:29:06 +0530

    added remote and local execution driver implementation

--- src/main/java/com/qaninjas/remotetools/Appium.java
+++ src/main/java/com/qaninjas/remotetools/Appium.java
@@ -1,5 +1,45 @@
 package com.qaninjas.remotetools;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.qaninjas.framework.constants.FrameworkConstants;
+
+
 public class Appium {
 
+	private WebDriver driver;
+	private static  Logger logger = Logger.getLogger(Appium.class);
+	
+	public WebDriver getAppiumDriver(DesiredCapabilities capabilities) {
+		if(FrameworkConstants.MOBILECONFIG.get("TARGET_MODE").equalsIgnoreCase("local")){
+			getAppiumLocalDriver(capabilities);
+		}else {
+			getAppiumGridDriver(capabilities);
+		}
+		return driver;
+	}
+
+	private void getAppiumGridDriver(DesiredCapabilities capabilities) {
+		try {
+			driver = new RemoteWebDriver(new URL(FrameworkConstants.MOBILECONFIG.get("REMOTE_URL")), capabilities);
+			logger.info("Appium Grid driver is initialized");
+		}catch(MalformedURLException e) {
+			logger.error("Error in Appium Local driver initialization" + e.getMessage());
+		}
+	}
+
+	private void getAppiumLocalDriver(DesiredCapabilities capabilities) {
+		try {
+			driver = new RemoteWebDriver(new URL(FrameworkConstants.MOBILECONFIG.get("REMOTE_URL")), capabilities);
+			logger.info("Appium Local driver is initialized");
+		}catch(MalformedURLException e) {
+			logger.error("Error Appium Local driver is initialization" + e.getMessage());
+		}
+	}
 }
--- src/main/java/com/qaninjas/remotetools/SeeTest.java
+++ src/main/java/com/qaninjas/remotetools/SeeTest.java
@@ -1,5 +1,64 @@
 package com.qaninjas.remotetools;
 
+import java.net.URL;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+
+import com.qaninjas.framework.constants.FrameworkConstants;
+
 public class SeeTest {
 
+	private WebDriver driver;
+	private static  Logger logger = Logger.getLogger(SeeTest.class);
+	
+	public WebDriver getSeeTestDriver(DesiredCapabilities capabilities) {
+		if(FrameworkConstants.MOBILECONFIG.get("TARGET_MODE").equalsIgnoreCase("local")){
+			getSeeTestLocalDriver(capabilities);
+		}else if(FrameworkConstants.MOBILECONFIG.get("TARGET_MODE").equalsIgnoreCase("grid")){
+			getSeeTestGridDriver(capabilities);
+		}else {
+			logger.info("Check Mobile config TARGET_MODE value");
+		}
+		return driver;
+	}
+
+	private WebDriver getSeeTestLocalDriver(DesiredCapabilities capabilities) {
+		try {
+			if(FrameworkConstants.MOBILECONFIG.get("").equalsIgnoreCase("android")) {
+				driver = new AndroidDriver<AndroidElement>(new URL(FrameworkConstants.MOBILECONFIG.get("REMOTE_URL")), capabilities);
+			}else if(FrameworkConstants.MOBILECONFIG.get("").equalsIgnoreCase("ios")) {
+				driver = new IOSDriver<IOSElement>(new URL(FrameworkConstants.MOBILECONFIG.get("REMOTE_URL")), capabilities);
+			}else {
+				logger.info("Check Mobile config PLATFORM_OS value");
+			}
+		}catch(Exception e) {
+			logger.error("Error in SeeTest Local driver initialization" + e.getMessage());
+		}
+		return driver;
+	}
+
+	private WebDriver getSeeTestGridDriver(DesiredCapabilities capabilities) {
+		String testName = FrameworkConstants.MOBILECONFIG.get("PLATFORM_TYPE") + "execution";
+		capabilities.setCapability("testName", testName);
+		capabilities.setCapability("accessKey", FrameworkConstants.MOBILECONFIG.get("ACCESS_KEY"));
+		capabilities.setCapability("nativeWebScreenshot", true);
+		try {
+			if(FrameworkConstants.MOBILECONFIG.get("").equalsIgnoreCase("android")) {
+				driver = new AndroidDriver<AndroidElement>(new URL(FrameworkConstants.MOBILECONFIG.get("CLOUD_URL")), capabilities);
+			}else if(FrameworkConstants.MOBILECONFIG.get("").equalsIgnoreCase("ios")) {
+				driver = new IOSDriver<IOSElement>(new URL(FrameworkConstants.MOBILECONFIG.get("CLOUD_URL")), capabilities);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			logger.error("Error in SeeTest Grid driver initialization" + e.getMessage());
+		}
+		return driver;
+	}
 }
