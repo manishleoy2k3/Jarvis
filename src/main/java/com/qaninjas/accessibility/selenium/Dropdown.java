30810ed9abb84f5c7a33240a4f15a8aea1b7d283
manishleoy2k3 <manishleoy2k3@gmail.com>
2019-09-17 22:30:20 +0530

    added selenium package in accessibility package

---
+++ src/main/java/com/qaninjas/accessibility/selenium/Dropdown.java
@@ -0,0 +1,93 @@
+package com.qaninjas.accessibility.selenium;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import com.aventstack.extentreports.Status;
+import com.qaninjas.driverfactory.DriverFactory;
+import com.qaninjas.framework.Report;
+import com.qaninjas.framework.interfaces.IDropdown;
+import com.qaninjas.framework.utility.selenium.Locator;
+import com.qaninjas.selenium.SeleniumUtility;
+import com.qaninjas.selenium.Synchronization;
+
+public class Dropdown implements IDropdown{
+
+	private static Dropdown instance = null;
+	protected WebDriver driver = DriverFactory.getInstance().getDriver();
+	private static  Logger logger = Logger.getLogger(Dropdown.class);
+	
+	protected Synchronization sync = Synchronization.getInstance();
+	private Report report = Report.getInstance();
+	protected Locator locator = Locator.getInstance();
+	
+	protected SeleniumUtility utility = SeleniumUtility.getInstance();
+	
+	List<WebElement> optionsList;
+	
+	protected Dropdown() {
+		
+	}
+		
+	public static Dropdown getInstance() {
+		if(null == instance) {
+			instance = new Dropdown();
+		}
+		return instance;
+	}
+
+	public List<String> getAllOptions() {
+		sync.waitForElement(locator.getLocator());
+		Select options = new Select(driver.findElement(locator.getLocator()));
+		
+		optionsList = options.getOptions();
+		List<String> listOfOptions = utility.convertWebElementToString(optionsList);
+		
+		logger.debug("Dropdown locator found..." + locator.getLocator());
+		return listOfOptions;
+	}
+
+	public void selectbyVisibleText(String value) {
+		sync.waitForElement(locator.getLocator());
+		Select options = new Select(driver.findElement(locator.getLocator()));
+		
+		options.selectByVisibleText(value);
+		
+		logger.debug("Element locator found..." + locator.getLocator());
+		report.log("Selected value by VisibileText", value, Status.PASS);
+	}
+
+	public void selectbyIndex(int index) {
+		sync.waitForElement(locator.getLocator());
+		Select options = new Select(driver.findElement(locator.getLocator()));
+		
+		options.selectByIndex(index);
+		
+		logger.debug("Element locator found..." + locator.getLocator());
+		report.log("Selected value by Index", "", Status.PASS);
+	}
+
+	public void selectbyValue(String value) {
+		sync.waitForElement(locator.getLocator());
+		Select options = new Select(driver.findElement(locator.getLocator()));
+		
+		options.selectByValue(value);
+		
+		logger.debug("Element locator found..." + locator.getLocator());
+		report.log("Selected value by Value", value, Status.PASS);		
+	}
+
+	public String getFirstSelectedOption() {
+		sync.waitForElement(locator.getLocator());
+		Select options = new Select(driver.findElement(locator.getLocator()));
+		
+		String firstValue = options.getFirstSelectedOption().getText();
+		report.log("Selected value by Value", firstValue, Status.PASS);
+		return firstValue;
+	}
+	
+}
---
+++ src/main/java/com/qaninjas/accessibility/selenium/Elements.java
@@ -0,0 +1,80 @@
+package com.qaninjas.accessibility.selenium;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.aventstack.extentreports.Status;
+import com.qaninjas.driverfactory.DriverFactory;
+import com.qaninjas.framework.Report;
+import com.qaninjas.selenium.Synchronization;
+
+public class Elements {
+
+	private static Elements instance = null;
+	protected WebDriver driver = DriverFactory.getInstance().getDriver();
+	private static  Logger logger = Logger.getLogger(Elements.class);
+	
+	protected Synchronization sync = Synchronization.getInstance();
+	private Report report = Report.getInstance();
+	
+	protected Elements() {
+		
+	}
+		
+	public static Elements getInstance() {
+		if(null == instance) {
+			instance = new Elements();
+		}
+		return instance;
+	}
+
+	public void sendKeys(By elementLocator, String value, String description) {
+		clear(elementLocator);
+		driver.findElement(elementLocator).sendKeys(value);
+		logger.debug("Enter value " + value + "" + elementLocator);
+		report.log(description, value, Status.PASS);
+	}
+	
+	private void clear(By elementLocator) {
+		sync.waitForElement(elementLocator);
+		driver.findElement(elementLocator).clear();	
+	}
+
+	public void click(By elementLocator, String description) {
+		sync.waitForElement(elementLocator);
+		driver.findElement(elementLocator).click();
+		report.log(description, "", Status.PASS);
+	}
+
+	public String getText(By elementLocator) {
+		sync.waitForElement(elementLocator);
+		logger.debug("Element locator get Text... " + driver.findElement(elementLocator).getText());
+		return driver.findElement(elementLocator).getText();
+	}
+	
+	public String getAttribute(By elementLocator, String attribute) {
+		sync.waitForElement(elementLocator);
+		logger.debug("Element locator get Text... " + driver.findElement(elementLocator).getAttribute(attribute));
+		return driver.findElement(elementLocator).getAttribute(attribute);
+	}
+	
+	public boolean isEnabled(By elementLocator) {
+		sync.waitForElement(elementLocator);
+		return driver.findElement(elementLocator).isEnabled();
+	}
+	
+	public boolean isDisplayed(By elementLocator) {
+		sync.waitForElement(elementLocator);
+		return driver.findElement(elementLocator).isDisplayed();
+	}
+	
+	public boolean isSelected(By elementLocator) {
+		sync.waitForElement(elementLocator);
+		return driver.findElement(elementLocator).isSelected();
+	}
+	
+	public void getURL(String url) {
+		driver.get(url);
+	}
+}
