790b46108e72194ac7b3107d4d72f965cf5e41aa
manishleoy2k3 <manishleoy2k3@gmail.com>
2019-09-26 09:57:17 +0530

    started adding test calsses

--- src/test/java/com/qaninjas/app/test/FrameworkAppTest.java
+++ src/test/java/com/qaninjas/app/test/FrameworkAppTest.java
@@ -1,5 +1,13 @@
 package com.qaninjas.app.test;
 
+import com.qaninjas.framework.RunTestNG;
+
 public class FrameworkAppTest {
+
+	private static RunTestNG testNG = new RunTestNG();
+	
+	public static void main(String[] args) {
+		testNG.executeTestNGXML("src/test/resources/xml/mobileapp.xml");
+	}
 
 }
